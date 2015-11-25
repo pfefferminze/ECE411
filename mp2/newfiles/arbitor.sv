6676eaf0b41b5fafd94d1d122fdb7d2b4a4ba679
Nick Moore <nickmooregot33@gmail.com>
2015-11-25 10:54:32 -0600

    got arbitor working

--- mp2/newfiles/arbitor.sv
+++ mp2/newfiles/arbitor.sv
@@ -1,30 +1,29 @@
-
 module arbitor(
-	input logic 		 clk,
-				 i_mem_write,		//
-				 d_mem_write,		//
-				 i_mem_read,		//
-				 d_mem_read,		//
-				 L2_resp,		//
+					input logic 		 	clk,
+												i_mem_write,		//
+												d_mem_write,		//
+												i_mem_read,		//
+												d_mem_read,		//
+												L2_resp,		//
 						
-	input logic [127:0]      L2_rdata,              //input from L2
-				 d_wdata, //data from dcache to L2 cache
+					input logic [127:0]	L2_rdata,              //input from L2
+												d_wdata, //data from dcache to L2 cache
 			   
-	output logic [127:0]     i_rdata,               //output to icache
-				 d_rdata,		//output to dcache
+					output logic [127:0]	i_rdata,               //output to icache
+												d_rdata,		//output to dcache
 						
-	input logic [15:0] 	 i_raddr,		//
-				 d_raddr,		//
+					input logic [15:0]	i_raddr,		//
+												d_raddr,		//
 						
-	output logic 		 L2_read,		//
-				 L2_write,		//
-				 i_resp,		//
-				 d_resp,		//
+					output logic 			L2_read,		//
+												L2_write,		//
+												i_resp,		//
+												d_resp,		//
 	
-	output logic [127:0] 	L2_wdata,		//from L1 to L2
+					output logic [127:0] L2_wdata,		//from L1 to L2
 						 
-	input logic [127:0]     i_wdata, 		//data from icache to L2 cache
-	output logic [15:0]  	L2_addr 		//address to send to L2
+					input logic [127:0] 	i_wdata, 		//data from icache to L2 cache
+					output logic [15:0] 	L2_addr 		//address to send to L2
 );
 
 enum {
@@ -40,8 +39,8 @@ enum {
 } req_type, next_req_type;
 
    logic reg_load;
-logic [15:0]  address_reg_in,address_reg_out;
-   
+logic [15:0]  address_out,address_mux_out;
+logic address_sel, addressmux_sel;   
 
 always_ff @ (posedge clk) begin
 	state <= next_state;
@@ -125,12 +124,12 @@ always_comb begin : state_actions
 		   reg_load = 1'b1;
 		end
 		WB: begin
+			reg_load = 1'b0;
 		   L2_write = 1;
-		   reg_load = 1'b0;			   
 			if(req_type == DATA) begin
 				d_resp = L2_resp;
 //				L2_addr = d_raddr;
-				L2_wdata = d_rdata;
+				L2_wdata = d_wdata;
 			end
 			else begin
 				i_resp = L2_resp;
@@ -154,19 +153,25 @@ always_comb begin : state_actions
 end : state_actions
 
 
-   assign address_reg_in = (req_type == DATA)? d_raddr:i_raddr;
-   assign L2_addr = address_reg_out;
+   assign address_sel = (req_type == DATA)? 1'b0:1'b1;
+   assign L2_addr = address_mux_out;
    
-   register address_register(
-//    input 
-							  .clk(clk),
-//    input 
-							  .load(reg_load),
-//    input [width-1:0] 
-							  .in(address_reg_in),
-//    output logic [width-1:0] 
-							  .out(address_reg_out)
+   register #(1) address_register(
+/*    input*/					  .clk(clk),
+/*    input */					  .load(reg_load),
+/*    input [width-1:0]*/	  .in(address_sel),
+/* output logic [width-1:0]*/.out(addressmux_sel)
 );
 
+mux2 #(16) address_mux(
+/*input*/ 							.sel(addressmux_sel),	
+/*input [width-1:0]*/ 			.a(d_raddr), .b(i_raddr),
+/*output logic [width-1:0]*/ 	.f(address_mux_out)
+);
    
+mux2 #(16) data_mux(
+/*input*/ 							.sel(addressmux_sel),	
+/*input [width-1:0]*/ 			.a(d_raddr), .b(i_raddr),
+/*output logic [width-1:0]*/ 	.f(data_mux_out)
+);
 endmodule
